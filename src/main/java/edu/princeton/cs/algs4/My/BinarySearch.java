61b3128588900afb9a1f6ae7b5057ecc0e5465f8
yuxiayiwanshang <yuxiayiwanshang@163.com>
2018-11-23 19:14:53 +0800

    Update BinarySearch.java

--- src/main/java/edu/princeton/cs/algs4/My/BinarySearch.java
+++ src/main/java/edu/princeton/cs/algs4/My/BinarySearch.java
@@ -1,3 +1,21 @@
 public class BinarySearch{
+    public BinarySearch(){
+    }
     
+    public static int indexOf(int[] a, int key){
+        int lo=0;
+        int hi=a.length -1;
+        while(lo<=hi){
+            int mid = ( lo+ hi)/2;
+            if(key > a[mid] ) lo = mid +1;
+            else if(key < a[mid] ) hi = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args){
+        int[] a = {1,4,7,11,15,23};
+        System.out.println(indexOf(a,7));
+    }
 }
