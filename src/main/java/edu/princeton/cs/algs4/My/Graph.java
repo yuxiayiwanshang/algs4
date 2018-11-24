29f9fbbb686fc95fbb649b46d877606843e72df1
yuxiayiwanshang <yuxiayiwanshang@163.com>
2018-11-24 23:47:49 +0800

    Update Graph.java

--- src/main/java/edu/princeton/cs/algs4/My/Graph.java
+++ src/main/java/edu/princeton/cs/algs4/My/Graph.java
@@ -56,7 +56,7 @@ public class Graph{
   
   public static void main(String[] args){
     int v = 8;
-    int[] edges = {1,2,1,3,2,4,2,5,3,6,3,7);
+    int[] edges = {1,2,1,3,2,4,2,5,3,6,3,7};
     Graph graph = new Graph(v,edges);
     System.out.println(graph.toString());
   }
